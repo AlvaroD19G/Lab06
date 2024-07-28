80d47d121a26230dd0e179b72f647866888b7cb8
AlvaroD19G <alduartega@est.utn.ac.cr>
2024-07-28 17:24:05 -0600

    Login

---
+++ Login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style/Login.css">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="Container">
+        <form class="form">
+            <div class="title">LOGIN<br></div>
+            <input class="input" name="email" placeholder="Email" type="email">
+            <input class="input" name="password" placeholder="Password" type="password">
+            <img style="width: 100px; height:100px;"
+                src="img/5258525_blackboard_classroom_study_teacher_writing_icon.png" alt="">
+            <button class="button-confirm">Let`s go →</button>
+        </form>
+    </div>
+</body>
+
+</html>
